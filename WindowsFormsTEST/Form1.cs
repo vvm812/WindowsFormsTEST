648fcbe05d648e462cf498556cbfa619a9c7aed1
vvm vvm <vvm-investment@mail.ru>
2018-12-14 19:21:19 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsTEST/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsTEST
+{
+    partial class MyButtonClass : Form
+    {
+        private Button mrButton;
+
+        public MyButtonClass()
+        {
+            InitializeComponent();
+            mrButton = new Button();
+            mrButton.Text = "Нажми на меня";
+            mrButton.Top = 100;
+            mrButton.Left = 100;
+            mrButton.Height = 50;
+            mrButton.Width = 70;
+            mrButton.Click += MrButton_Click;
+            mrButton.MouseDown += MrButton_MouseDown;
+            this.Controls.Add(mrButton);
+        }
+
+        private void MrButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void MrButton_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                this.Text = "Нажата левая кнопка мыши";
+        }
+    }
+}
